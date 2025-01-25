9a2a7a66d34374273f272c9bcd556eaa0657b2ec
Bogdyans <BOGDYA2004@gmail.com>
2025-01-25 20:51:00 +0300

    feat(bogy): disable canon mode

--- bogy.c
+++ bogy.c
@@ -1,4 +1,32 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+struct termios originalTermios;
+
+void disableRawMode()
+{
+    tcsetattr( STDIN_FILENO, TCSAFLUSH, &originalTermios );
+}
+
+void enableRawMode()
+{
+    tcgetattr( STDIN_FILENO, &originalTermios );
+    struct termios raw = originalTermios;
+
+    raw.c_lflag &= ~( ECHO | ICANON );
+
+    tcsetattr( STDIN_FILENO, TCSAFLUSH, &raw );
+}
+
 int main()
 {
+    enableRawMode();
+    atexit( disableRawMode );
+
+    char character;
+
+    while ( read( STDIN_FILENO, &character, 1 ) == 1 && character != 'q' );
+
     return 0;
 }
